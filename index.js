7df434b7f3d523c5f7b090254b92dae8c374ce73
cpmangukiya <chintanpmangukiya@gmail.com>
2019-09-14 16:12:57 +0530

    interims

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 module.exports = function () {
     let r = {}
+
+    const subProcess = require("./sub-process");
+
+    r.doSomething = null;
+
     r.isEmpty = function (input) {
         // Returns true if the input is either undefined, null, empty, {} or []. false otherwise.
         return (input === undefined || input === null || input === '' || input === {} || input === [])
     }
-    r.doSomething = null;
+
+    r.launchServer = function () {
+        subProcess.makeUpAppServer();
+    }
+
     return r
 }();
